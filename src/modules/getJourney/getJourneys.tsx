f713d385935bcf6ba7ca5e132ae4c80f32036563
William Byrne <will.bl.byrne@gmail.com>
2025-08-23 21:42:24 +0100

    Remove basic auth

--- src/modules/getJourney/getJourneys.tsx
+++ src/modules/getJourney/getJourneys.tsx
@@ -1,16 +1,12 @@
 export default async function getJourneys(dept: string, dest: string) {
     // env
-    const apiUrl = process.env.NEXT_PUBLIC_REALTIME_TRAINS_ENDPOINT || "";
-    const username = process.env.NEXT_PUBLIC_USERNAME || "";
-    const password = process.env.NEXT_PUBLIC_PASSWORD || "";
-    const urlString = ( apiUrl ?? "http://localhost:8080") +`json/services/${dept}/to/${dest}`;
-    // const urlString = ( "http://localhost:8080/") +`services/${dept}/to/${dest}`;
+    const apiUrl = process.env.NEXT_PUBLIC_API_ENDPOINT || "";
+    const urlString = ( apiUrl ?? "http://localhost:8080") +`services/${dept}/to/${dest}`;
 
+    console.log("apiurl", apiUrl)
 
     try {
-        const response = await fetchWithBasicAuth(urlString, username, password);
-        const response2 = await getServicesByRoute(dept, dest);
-        console.log("response2", response2);
+        const response = await fetch(urlString)
         const json = await response.json()
         return json;
 
@@ -19,35 +15,4 @@ export default async function getJourneys(dept: string, dest: string) {
         return [{"name": "error fetching"}, []]
     }
 
-}
-const getServicesByRoute = async (dept: string, dest: string) => {
-    const apiUrl = process.env.NEXT_PUBLIC_REALTIME_TRAINS_ENDPOINT || "";
-    const username = process.env.NEXT_PUBLIC_USERNAME || "";
-    const password = process.env.NEXT_PUBLIC_PASSWORD || "";
-    const urlString = ( apiUrl ?? "http://localhost:8080") +`services/${dept}/to/${dest}`;
-    // const urlString = ( "http://localhost:8080/") +`services/${dept}/to/${dest}`;
-    return await fetchWithBasicAuth(urlString, username, password);
-}
-
-
-async function fetchWithBasicAuth(
-    url: string,
-    username: string,
-    password: string
-) {
-    const credentials = btoa(`${username}:${password}`);
-    try {
-        const response = await fetch(url, {
-            headers: {
-                Authorization: `Basic ${credentials}`,
-            },
-        });
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-        return { error: "Error fetching with basic auth" };
-    }
 }
